1edee19726ad821da24893f95de9f2f12fdf1bab
Tejas15sutar <tejassutar1507@gmail.com>
2026-03-28 17:35:12 +0530

    Clean project

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,6 @@ urlpatterns=[
     path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('<int:movie_id>/theaters',views.theater_list,name='theater_list'),
     path('theater/<int:theater_id>/seats/book/',views.book_seats,name='book_seats'),
-    path('confirm-booking/<int:seat_id>/', views.confirm_booking, name='confirm_booking'),
     path('create-payment/', views.create_payment, name='create_payment'),
     path("payment/webhook/", views.payment_webhook),
     path('payment-success/', views.payment_success, name='payment_success'),
